26a1b90c0a80ad33c519541730ef9a537a718e11
Ephraim Härer <ephraim.haerer@renolit.com>
2020-09-17 21:49:53 +0200

    initial update

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,33 +8,33 @@
  * writing. "version" and "dependencies" must not be touched!
  * ************************************************************* */
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Powermail Country Viewhelper',
-	'description' => 'An extended viewhelper for the powermail country select field.',
-	'category' => 'plugin',
-	'author' => 'Ephraim Härer',
-	'author_email' => 'ephraim.haerer@renolit.com',
-	'author_company' => 'RENOLIT SE',
-	'state' => 'stable',
-	'uploadfolder' => false,
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '1.3.0',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '7.6.0-7.99.99',
-			'php' => '5.5.0-7.1.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'clearcacheonload' => false,
-	'psr-4' => array(
-		'classmap' => array(
-			'RENOLIT\\ReintPowermailCountry\\' => 'Classes'
-		),
-	),
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Powermail Country Viewhelper',
+    'description' => 'An extended ViewHelper for the powermail country select field.',
+    'category' => 'plugin',
+    'author' => 'Ephraim Härer',
+    'author_email' => 'ephraim.haerer@renolit.com',
+    'author_company' => 'RENOLIT SE',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'createDirs' => '',
+    'clearCacheOnLoad' => false,
+    'version' => '2.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '9.5.17-10.4.99',
+            'static_info_tables' => '6.9.0-6.9.99',
+            'php' => '7.2.0-0.0.0',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+    'psr-4' => [
+        'classmap' => [
+            'RENOLIT\\ReintPowermailCountry\\' => 'Classes'
+        ],
+    ],
+];
 
